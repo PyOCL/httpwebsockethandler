c8253219ad1eaae93b3761af7ef99e8d0395c405
Kilik Kuo <kikuo@mozilla.com>
2017-03-12 21:58:32 +0800

    Make it compatible with Python2&3.

--- ExampleWSServer.py
+++ ExampleWSServer.py
@@ -1,11 +1,19 @@
-#main program and imports for standalone purpose       
+#main program and imports for standalone purpose
 import sys
 import threading
-from SocketServer import ThreadingMixIn
-from BaseHTTPServer import HTTPServer
-from SimpleHTTPServer import SimpleHTTPRequestHandler
 import ssl
 from base64 import b64encode
+
+VER = sys.version_info[0]
+if VER >= 3:
+    from socketserver import ThreadingMixIn
+    from http.server import HTTPServer
+    from io import StringIO
+else:
+    from SocketServer import ThreadingMixIn
+    from BaseHTTPServer import HTTPServer
+    from StringIO import StringIO
+
 from HTTPWebSocketsHandler import HTTPWebSocketsHandler
 
 if len(sys.argv) > 1:
@@ -26,7 +34,7 @@ class WSSimpleEcho(HTTPWebSocketsHandler):
         if message is None:
             message = ''
         # echo message back to client
-        self.send_message(str(message))
+        self.send_message(message)
         self.log_message('websocket received "%s"',str(message))
 
     def on_ws_connected(self):
@@ -37,18 +45,18 @@ class WSSimpleEcho(HTTPWebSocketsHandler):
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     """Handle requests in a separate thread."""
-   
+
 def _ws_main():
     try:
         #Replace WSSimpleEcho with your own subclass of HTTPWebSocketHandler
         server = ThreadedHTTPServer(('', port), WSSimpleEcho)
         server.daemon_threads = True
-        server.auth = b64encode(credentials)
+        server.auth = b64encode(credentials.encode("ascii"))
         if secure:
-            server.auth = b64encode(credentials)
+            server.auth = b64encode(credentials.encode("ascii"))
             server.socket = ssl.wrap_socket (server.socket, certfile='./server.pem', server_side=True)
             print('started secure https server at port %d' % (port,))
-        else: 
+        else:
             print('started http server at port %d' % (port,))
         server.serve_forever()
     except KeyboardInterrupt:
